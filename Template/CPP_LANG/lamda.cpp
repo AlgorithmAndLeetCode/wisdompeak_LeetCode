29b67c9cc2cea5ad8dc8d64d6964207e3ab245d3
wisdompeak <guan.huifeng@gmail.com>
2026-06-02 02:24:57 -0700

    Update lamda.cpp

--- Template/CPP_LANG/lamda.cpp
+++ Template/CPP_LANG/lamda.cpp
@@ -1,11 +1,11 @@
-# 定义一般的lamda函数
-auto dfs = [&](int l, int r) {
+# 定义允许递归的的lamda函数
+std::function<void(int,int)> dfs = [&](int l, int r) {
   int mid = (l+r)/2;
   dfs(l, mid-1);
   dfs(mid, r));
 }
 
-# 定义允许递归的lamda函数
+# 定义允许递归的、带有返回值的lamda函数
 std::function<ll(int,int)> dfs = [&](int l, int r) -> ll {
     int mid = (l+r)/2;
     return min(dfs(l, mid-1) + dfs(mid, r));
