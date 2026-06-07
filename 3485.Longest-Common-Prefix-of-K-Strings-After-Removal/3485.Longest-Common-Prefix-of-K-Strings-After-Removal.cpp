fe89d3a07f9060cc58aa27b6e5e9e82a195954cc
wisdompeak <guan.huifeng@gmail.com>
2026-06-07 00:56:56 -0700

    Create 3485.Longest-Common-Prefix-of-K-Strings-After-Removal.cpp

---
+++ 3485.Longest-Common-Prefix-of-K-Strings-After-Removal/3485.Longest-Common-Prefix-of-K-Strings-After-Removal.cpp
@@ -0,0 +1,58 @@
+class TrieNode {
+    public:
+    TrieNode* next[26];   
+    int count = 0;
+    TrieNode()
+    {
+        for (int i=0; i<26; i++)
+            next[i]=NULL;        
+    }
+};
+
+class Solution {
+    TrieNode* root;
+    int k;
+    map<int,int>Map;
+public:
+    void add(string& s, int delta) {
+        TrieNode* node = root;
+        int level = 0;
+        for (auto ch: s) {
+            if (node->next[ch-'a']==NULL)
+                node->next[ch-'a']=new TrieNode();
+            node = node->next[ch-'a'];
+            node->count+=delta;
+            level++;
+
+            int oldValue = node->count-delta;
+            int newValue = node->count;
+            if (oldValue<k && newValue>=k) {
+                Map[level]++;
+            } else if (oldValue>=k && newValue<k) {
+                Map[level]--;
+                if (Map[level]==0)
+                    Map.erase(level);
+            }
+        }
+    }
+
+    vector<int> longestCommonPrefix(vector<string>& words, int k) {
+        this->k = k;
+        root = new TrieNode();
+        for (auto& word: words)
+            add(word, 1);        
+
+        vector<int>rets;
+        for (auto& word: words) {
+            add(word, -1);            
+
+            if (Map.empty())
+                rets.push_back(0);
+            else
+                rets.push_back(Map.rbegin()->first);
+            
+            add(word, 1);
+        }
+        return rets;
+    }
+};
